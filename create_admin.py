bbc8e5d1929ca07fc23e46a9df870a806ba6acca
Souradji <souradjimohamadou@gmail.com>
2026-01-23 11:25:21 +0100

    Update create_admin.py

--- create_admin.py
+++ create_admin.py
@@ -1,1 +1,22 @@
 
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
+
+from app_auth.models import User # Vérifie bien le chemin vers ton modèle User custom
+
+def create_superuser():
+    username = "admin"
+    email = "admin@example.com"
+    password = "1234" # Change-le ici !
+
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print(f"Superuser '{username}' créé avec succès.")
+    else:
+        print(f"L'utilisateur '{username}' existe déjà.")
+
+if __name__ == "__main__":
+    create_superuser()
